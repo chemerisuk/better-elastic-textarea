d998d1c857d74c647e21d0efcf5ec42e4b97d94c
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-11-24 17:44:31 +0300

    various fixes, add a link to the inspiration article

--- src/better-elastic-textarea.js
+++ src/better-elastic-textarea.js
@@ -3,25 +3,30 @@
 
     var SPACE_HOLDER_KEY = "space-holder";
 
+    // Insiped by article at a list apart:
+    // http://alistapart.com/article/expanding-text-areas-made-elegant
+
     DOM.extend("textarea[rows='1']", {
         constructor: function() {
-            var wrapper = DOM.create("div.better-elastic-textarea>pre[style=\"font:${font};padding:${pad};border-width:${bw}\"]>span", {
-                font: this.style("font"),
-                pad: this.style("padding"),
-                bw: this.style("border-width")
-            });
+            var wrapper = DOM.create("div.better-elastic-textarea>pre>span");
 
             this
                 .data(SPACE_HOLDER_KEY, wrapper.find("span"))
-                .on("input", "handleInput");
+                .on("input", this.onTextareaInput);
 
-            this.parent("form").on("reset", this, "handleFormReset");
+            this.parent("form").on("reset", this, this.onFormReset);
 
-            wrapper.append(this.after(wrapper).set("rows", null));
+            wrapper.child(0).style({
+                font: this.style("font"),
+                padding: this.style("padding"),
+                "border-width": this.style("border-width")
+            });
+
+            wrapper.append(this.after(wrapper));
         },
-        handleInput: function(value) {
-            // use a small trick here: type of the first
-            // argument is string only when we send defaultValue
+        onTextareaInput: function(value) {
+            // use a small trick here: type of the first argument
+            // is string only when defaultValue is sent in handleFormReset
             if (typeof value !== "string") value = this.get();
 
             // use &nbsp; to fix issue with adding a new line
@@ -30,7 +35,7 @@
             // IE doesn't respect newlines so use <br> instead
             this.data(SPACE_HOLDER_KEY).set(value.split("\n").join("<br>"));
         },
-        handleFormReset: function() {
+        onFormReset: function() {
             this.fire("input", this.get("defaultValue"));
         }
     });
