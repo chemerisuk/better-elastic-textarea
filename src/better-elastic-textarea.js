d7135a8733ade8450e52e782474d6ab830cebdc9
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-11-30 15:50:26 +0300

    minor refactoring + add tests

--- src/better-elastic-textarea.js
+++ src/better-elastic-textarea.js
@@ -12,7 +12,7 @@
 
             this
                 .data(SPACE_HOLDER_KEY, wrapper.find("span"))
-                .on("input", this.onTextareaInput);
+                .on("input", this.onInput);
 
             this.parent("form").on("reset", this, this.onFormReset);
 
@@ -25,10 +25,9 @@
 
             wrapper.append(this.after(wrapper));
         },
-        onTextareaInput: function(value) {
-            // use a small trick here: type of the first argument
-            // is string only when defaultValue is sent in handleFormReset
-            if (typeof value !== "string") value = this.get();
+        onInput: function(value) {
+            // use textarea value in regular input event case
+            if (arguments.length < 3) value = this.get();
 
             // use &nbsp; to fix issue with adding a new line
             if (value[value.length - 1] === "\n") value += "&nbsp;";
