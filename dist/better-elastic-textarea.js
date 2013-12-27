cdb558af9939cf8e78a36e21e3aa8c8565f7ff71
Maksim Chemerisuk <chemerisuk@gmail.com>
2013-12-27 09:04:24 +0300

    version 1.1.0

--- dist/better-elastic-textarea.js
+++ dist/better-elastic-textarea.js
@@ -1,12 +1,12 @@
 /**
  * @file src/better-elastic-textarea.js
- * @version 1.1.0 2013-12-03T15:50:44
+ * @version 1.1.0 2013-12-27T09:04:24
  * @overview Elastic textarea for better-dom
  * @copyright Maksim Chemerisuk 2013
  * @license MIT
  * @see https://github.com/chemerisuk/better-elastic-textarea
  */
-(function(DOM, SPACE_HOLDER_KEY) {
+(function(DOM) {
     "use strict";
 
     // Insiped by article at a list apart:
@@ -16,11 +16,9 @@
         constructor: function() {
             var wrapper = DOM.create("div.better-elastic-textarea>pre>span");
 
-            this
-                .data(SPACE_HOLDER_KEY, wrapper.find("span"))
-                .on("input", this.onInput);
+            this.on("input", this.onInput.bind(this, wrapper.find("span")));
 
-            this.parent("form").on("reset", this, this.onFormReset);
+            this.parent("form").on("reset", this.onFormReset.bind(this));
 
             wrapper.child(0).style({
                 font: this.style("font"),
@@ -31,18 +29,18 @@
 
             wrapper.append(this.after(wrapper));
         },
-        onInput: function(value) {
+        onInput: function(holder, value) {
             // use textarea value in regular input event case
-            if (arguments.length < 3) value = this.get();
+            if (arguments.length < 4) value = this.get();
 
             // use &nbsp; to fix issue with adding a new line
             if (value[value.length - 1] === "\n") value += "&nbsp;";
 
             // IE doesn't respect newlines so use <br> instead
-            this.data(SPACE_HOLDER_KEY).set(value.split("\n").join("<br>"));
+            holder.set(value.split("\n").join("<br>"));
         },
         onFormReset: function() {
             this.fire("input", this.get("defaultValue"));
         }
     });
-}(window.DOM, "space-holder"));
+}(window.DOM));
