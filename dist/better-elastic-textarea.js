0fb824c894112fd6a1e8422996c4dcfde6d24c45
Maksim Chemerisuk <chemerisuk@gmail.com>
2017-08-15 16:24:31 +0300

    1.2.0

--- dist/better-elastic-textarea.js
+++ dist/better-elastic-textarea.js
@@ -1,46 +1,41 @@
 /**
- * @file src/better-elastic-textarea.js
- * @version 1.1.1 2013-12-27T09:08:17
- * @overview Elastic textarea for better-dom
- * @copyright Maksim Chemerisuk 2013
+ * better-elastic-textarea: Elastic textarea for better-dom
+ * @version 1.2.0 Tue, 15 Aug 2017 13:24:30 GMT
+ * @link https://github.com/chemerisuk/better-elastic-textarea
+ * @copyright 2017 Maksim Chemerisuk
  * @license MIT
- * @see https://github.com/chemerisuk/better-elastic-textarea
  */
-(function(DOM) {
+(function (DOM) {
     "use strict";
 
     // Insiped by article at a list apart:
     // http://alistapart.com/article/expanding-text-areas-made-elegant
 
     DOM.extend("textarea[rows='1']", {
-        constructor: function() {
-            var wrapper = DOM.create("div.better-elastic-textarea>pre>span");
+        constructor: function () {
+            var wrapper = DOM.create("<div class='better-elastic-textarea'><pre><span></span></pre></div>");
+            var holder = wrapper.find("span");
 
-            this.on("input", this.onInput.bind(this, wrapper.find("span")));
+            this.on("input", this._inputTextarea.bind(this, holder));
+            this.closest("form").on("reset", this._inputTextarea.bind(this, holder, "defaultValue"));
 
-            this.parent("form").on("reset", this.onFormReset.bind(this));
-
-            wrapper.child(0).style({
-                font: this.style("font"),
-                margin: this.style("margin"),
-                padding: this.style("padding"),
-                "border-width": this.style("border-width")
+            wrapper.child(0).css({
+                font: this.css("font"),
+                margin: this.css("margin"),
+                padding: this.css("padding"),
+                "border-width": this.css("border-width")
             });
 
             wrapper.append(this.after(wrapper));
         },
-        onInput: function(holder, value) {
+        _inputTextarea: function (holder, propName) {
             // use textarea value in regular input event case
-            if (arguments.length < 4) value = this.get();
-
+            var value = propName ? this.get(propName) : this.value();
             // use &nbsp; to fix issue with adding a new line
             if (value[value.length - 1] === "\n") value += "&nbsp;";
-
             // IE doesn't respect newlines so use <br> instead
-            holder.set(value.split("\n").join("<br>"));
-        },
-        onFormReset: function() {
-            this.fire("input", this.get("defaultValue"));
+            holder.set("innerHTML", value.split("\n").join("<br>"));
         }
     });
-}(window.DOM));
+})(window.DOM);
+DOM.importStyles("@media screen", "textarea[rows='1']{display:block;width:100%;resize:none;-webkit-box-sizing:border-box;box-sizing:border-box}.better-elastic-textarea{position:relative}.better-elastic-textarea>textarea{position:absolute;left:0;top:0;height:100%;overflow:hidden;margin:0}.better-elastic-textarea>pre{visibility:hidden;margin:0;border-style:solid}.better-elastic-textarea span{display:inline-block;white-space:pre-wrap}");
